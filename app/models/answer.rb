0b2ebf0151e7e6d993263f79fc27e96fc4719bd9
Petro Koriakin <pkor@softserveinc.com>
2015-08-01 18:57:44 +0300

    make test pass

--- app/models/answer.rb
+++ app/models/answer.rb
@@ -1,3 +1,5 @@
 class Answer < ActiveRecord::Base
   belongs_to :question
+  validates :body, presence: true, length: {in: 3..1000}
+  validates :question_id, presence: true
 end
--- app/models/question.rb
+++ app/models/question.rb
@@ -1,3 +1,4 @@
 class Question < ActiveRecord::Base
   has_many :answers
+  validates :title, presence: true, length: {in: 3..256}
 end
